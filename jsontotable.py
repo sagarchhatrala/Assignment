b7cf1ee85ffc8d7383b783db0f1f5cee9f781274
sagarchhatrala <chhatralasagar99@gmail.com>
2022-01-17 13:10:20 +0530

    Removed extra spaces and comments

--- jsontotable.py
+++ jsontotable.py
@@ -11,13 +11,10 @@ for value in data['aws_s3_bucket']:
     if value['config']['acl'] != 'private':
         print("Acl is not private for: {}".format(value['id'])) 
 
-
-
 # S3 Bicket IDs where Bucket policy has Principal = “*”
 
 for value in data['aws_s3_bucket_policy']:
     a = json.loads(value['config']['policy'])
-    # print(a)
     
     for b in a['Statement']:
         if b['Principal'] == '*':
